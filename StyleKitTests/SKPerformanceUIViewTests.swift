470febbb18cd9ace36eab0162257835f3d9792eb
Mikhail Motylev <mmotylev@avito.ru>
2016-11-02 11:12:37 +0300

    UIView performance tests

---
+++ StyleKitTests/SKPerformanceUIViewTests.swift
@@ -0,0 +1,67 @@
+//
+//  SKPerformanceTests.swift
+//  SKStyleKit
+//
+//  Created by Мотылёв Михаил on 02/11/2016.
+//  Copyright © 2016 mmotylev. All rights reserved.
+//
+
+import XCTest
+@testable import SKStyleKit
+
+let viewsCount = 500
+
+class SKPerformanceUIViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        basicSetup()
+    }
+    
+    func testPerformanceUIViewFull() {
+        
+        // given
+        let style = StyleKit.style(withName: "viewStyle")
+        let views = (0 ..< viewsCount).map({ _ in SKView() })
+        
+        // when
+        self.measure {
+        
+            for view in views {
+                view.style = style
+            }
+        }
+    }
+    
+    func testPerformanceUIViewExceptShadow() {
+        
+        // given
+        let style = StyleKit.style(withName: "viewStyleExceptShadow")
+        let views = (0 ..< viewsCount).map({ _ in SKView() })
+        
+        // when
+        self.measure {
+            
+            for view in views {
+                view.style = style
+            }
+        }
+    }
+    
+    func testPerformanceUIViewExceptShadowBorderCorner() {
+        
+        // given
+        let style = StyleKit.style(withName: "viewStyleExceptShadowBorderCorner")
+        let views = (0 ..< viewsCount).map({ _ in SKView() })
+        
+        // when
+        self.measure {
+            
+            for view in views {
+                view.style = style
+            }
+        }
+    }
+    
+}
