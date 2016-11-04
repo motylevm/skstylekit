176322e46f83e41f0d479c8f72a7a2310859d537
Mikhail Motylev <mmotylev@avito.ru>
2016-11-04 13:15:34 +0300

    Demo project updated

--- Demo/StyleKitDemo/ViewController.swift
+++ Demo/StyleKitDemo/ViewController.swift
@@ -10,16 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func onTap(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
