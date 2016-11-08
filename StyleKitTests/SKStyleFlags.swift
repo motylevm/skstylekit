dc5b5aeacce68b51e0aa37676b3b5bcbebb5dbfa
MIKHAIL <revenantdr@mail.ru>
2016-11-08 07:48:50 +0300

    New tests

---
+++ StyleKitTests/SKStyleFlags.swift
@@ -0,0 +1,57 @@
+//
+//    Copyright (c) 2016 Mikhail Motylev https://twitter.com/mikhail_motylev
+//
+//    Permission is hereby granted, free of charge, to any person obtaining a copy of
+//    this software and associated documentation files (the "Software"), to deal in
+//    the Software without restriction, including without limitation the rights to
+//    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//    the Software, and to permit persons to whom the Software is furnished to do so,
+//    subject to the following conditions:
+//
+//    The above copyright notice and this permission notice shall be included in all
+//    copies or substantial portions of the Software.
+//
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+import XCTest
+@testable import SKStyleKit
+
+class SKStyleFlags: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        
+        basicSetup()
+    }
+    
+    func testFlagSetView() {
+        
+        // given
+        let styles = ["viewStyle", "viewStyleExceptShadow", "viewStyleExceptShadowBorderCorner"].map({ StyleKit.style(withName: $0)! })
+        let expectedFlags = [viewAllFlags, viewAllFlags ^ viewShadowFlag, viewAllFlags ^ viewShadowFlag ^ viewBorderFlag]
+        
+        // then
+        let flags = styles.map({ $0.flags })
+        
+        XCTAssertEqual(flags, expectedFlags)
+    }
+    
+    func testFlagSetLabel() {
+        
+        // given
+        let styles = ["labelCommon", "labelAdvanced"].map({ StyleKit.style(withName: $0)! })
+        let expectedFlags = [labelCommonFlag, labelAllFlags]
+        
+        // then
+        let flags = styles.map({ $0.flags })
+        
+        XCTAssertEqual(flags, expectedFlags)
+        
+    }
+}
